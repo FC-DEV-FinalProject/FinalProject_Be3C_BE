b810c26bb3e0a69fdda3a87503100cbab046f8c3
KJU3 <kju_3@naver.com>
2024-11-01 18:05:47 +0900

    init: 프로젝트 생성 및 초기화

---
+++ src/test/java/com/be3c/sysmetic/SysmeticApplicationTests.java
@@ -0,0 +1,11 @@
+package com.be3c.sysmetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysmeticApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
