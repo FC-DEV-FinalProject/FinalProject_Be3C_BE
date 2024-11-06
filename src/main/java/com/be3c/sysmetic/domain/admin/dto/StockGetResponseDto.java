578835f8dbbab959761b41dc52380f18c7c0f8a5
Eseas <8snlf9@gmail.com>
2024-11-06 20:18:33 +0900

    feat: 종목 엔티티 변경
    완료
    - 엔티티 클래스 변경

---
+++ src/main/java/com/be3c/sysmetic/domain/admin/dto/StockGetResponseDto.java
@@ -0,0 +1,17 @@
+package com.be3c.sysmetic.domain.admin.dto;
+
+import com.be3c.sysmetic.domain.admin.entity.Stock;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class StockGetResponseDto {
+    private Long id;
+    private String name;
+    private String filepath;
+}
