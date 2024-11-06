86b62cbdfe99483bd97a473b64a43b014a39e154
Eseas <8snlf9@gmail.com>
2024-11-06 19:06:34 +0900

    feat: 종목 컨트롤러 수정
    완료
    - Exception으로 반환 분기 설정
    - 페이지 객체 사용으로 변경
    - 종목 삭제 기능 추가

--- src/main/java/com/be3c/sysmetic/domain/admin/controller/StockController.java
+++ src/main/java/com/be3c/sysmetic/domain/admin/controller/StockController.java
@@ -1,18 +1,19 @@
 package com.be3c.sysmetic.domain.admin.controller;
 
-import com.be3c.sysmetic.domain.admin.dto.StockRequestDto;
+import com.be3c.sysmetic.domain.admin.dto.StockGetResponseDto;
+import com.be3c.sysmetic.domain.admin.dto.StockPutRequestDto;
 import com.be3c.sysmetic.domain.admin.entity.Stock;
 import com.be3c.sysmetic.domain.admin.service.StockService;
 import com.be3c.sysmetic.global.common.response.ApiResponse;
 import com.be3c.sysmetic.global.common.response.ErrorCode;
-import com.be3c.sysmetic.global.util.security.CustomUserDetails;
+import com.be3c.sysmetic.global.util.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +21,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Controller
@@ -30,6 +32,12 @@ public class StockController {
     private StockService stockService;
 
     /*
+        추가 필요 사항
+        1. S3에 아이콘 업로드 / DB 수정 코드
+        2. Global Exception Handler - AuthorizationException 처리 핸들러 필요.
+     */
+
+    /*
         종목명 중복 검사 메서드
      */
 //    @PreAuthorize(("hasRole('ADMIN')"))
@@ -37,13 +45,12 @@ public class StockController {
     public ResponseEntity<ApiResponse<String>> getCheckName(
         @RequestParam String name
     ) throws Exception {
-        if(stockService.findItemByName(name).isPresent()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(ApiResponse.fail(ErrorCode.BAD_REQUEST, "중복된 종목명입니다."));
+        if(stockService.duplcheck(name)) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.success("사용 가능한 종목명입니다."));
         }
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success("사용 가능한 종목명입니다."));
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(ApiResponse.fail(ErrorCode.DUPLICATE_RESOURCE, "중복된 종목명입니다."));
     }
 
     /*
@@ -51,23 +58,25 @@ public class StockController {
      */
     //@PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin/stock/{stock}")
-    public ResponseEntity<ApiResponse<Stock>> getitem(
+    public ResponseEntity<ApiResponse<StockGetResponseDto>> getitem(
             @PathVariable Long stock
     ) throws Exception {
-        if(stock == null) {
-            Optional<Stock> findResult = stockService.findItemById(stock);
+        try {
+            Stock find_stock = stockService.findItemById(stock);
 
-            if(findResult.isPresent()) {
-                // 아이콘 찾는 코드 추가 필요.
-                Stock findStock = findResult.get();
-                stockService.findItemIcon(findStock.getId());
+            StockGetResponseDto stockGetResponseDto = StockGetResponseDto.builder()
+                    .id(find_stock.getId())
+                    .name(find_stock.getName())
+                    // 아이콘 찾는 코드 추가 필요.
+                    // .filepath()
+                    .build();
 
-                return ResponseEntity.status(HttpStatus.OK)
-                        .body(ApiResponse.success());
-            }
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.success(stockGetResponseDto));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(ApiResponse.fail(ErrorCode.BAD_REQUEST, "해당 종목을 찾을 수 없습니다."));
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.fail(ErrorCode.BAD_REQUEST, "해당 종목을 찾을 수 없습니다."));
     }
 
     /*
@@ -76,19 +85,22 @@ public class StockController {
      */
     // @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin/stock")
-    public ResponseEntity<ApiResponse<Page<List<Stock>>>> getAllitem(
+    public ResponseEntity<ApiResponse<Page<Stock>>> getStockPage(
+    //나중에 따로 Dto 생성해야함!
+    //public ResponseEntity<ApiResponse<Page<StockPageGetRequestDto>>> getStockPage(
             @RequestParam Integer page
     ) throws Exception {
-        Page<List<Stock>> arrayStock = stockService.findItemPage(page);
+        try {
+            Page<Stock> stock_page = stockService.findItemPage(page);
 
-        if(arrayStock.isEmpty()) {
+            // 아이콘 찾기 필요.
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.success(stock_page));
+        } catch (NoSuchElementException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(ApiResponse.fail(ErrorCode.BAD_REQUEST, "페이지를 확인해주세요."));
+                    .body(ApiResponse.fail(ErrorCode.BAD_REQUEST, "해당 종목을 찾을 수 없습니다."));
         }
-        // 아이콘 찾는 코드 추가 필요.
-
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(arrayStock));
     }
 
     /*
@@ -97,7 +109,7 @@ public class StockController {
     // @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/admin/stock")
     public ResponseEntity<ApiResponse<Integer>> saveitem(
-            @RequestBody StockRequestDto stockRequestDto
+            @RequestBody StockPutRequestDto stockRequestDto
     ) throws Exception {
         try {
             if(stockRequestDto.getCheckDuplicate()) {
@@ -107,34 +119,37 @@ public class StockController {
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
             Object principal = authentication.getPrincipal();
 
-            if(principal instanceof UserDetails) {
-                UserDetails userDetails = (UserDetails) principal;
+            if(principal instanceof UserDetails userDetails) {
                 Long userId = ((CustomUserDetails) userDetails).getUserId();
 
                 stockService.saveItem(stockRequestDto, userId);
             }
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.success());
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(ApiResponse.fail(ErrorCode.BAD_REQUEST, "잘못된 값입니다."));
         }
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success());
     }
 
     /*
         종목 수정하기
      */
     // @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/admin/stock/{stock}")
-    public ResponseEntity<ApiResponse<String>> updateItem(@PathVariable("stock") String stock,
-                           @RequestBody StockRequestDto stockRequestDto
+    @PutMapping("/admin/stock")
+    public ResponseEntity<ApiResponse<String>> updateItem(
+            @RequestBody StockPutRequestDto stockPutRequestDto
     ) throws Exception {
         try {
-            if(stockRequestDto.getCheckDuplicate()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(ApiResponse.fail(ErrorCode.BAD_REQUEST, "잘못된 요청입니다."));
-            }
-            if(stockService.updateItem(stockRequestDto)) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication.getPrincipal();
+
+            if(principal instanceof UserDetails userDetails) {
+                Long userId = ((CustomUserDetails) userDetails).getUserId();
+
+                stockService.updateItem(stockPutRequestDto, userId);
+
                 return ResponseEntity.status(HttpStatus.OK)
                         .body(ApiResponse.success());
             }
@@ -150,14 +165,28 @@ public class StockController {
     }
 
     // @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/admin/stock")
-    public void deleteItem(
-            @RequestBody StockRequestDto stockRequestDto
+    @DeleteMapping("/admin/stock/{id}")
+    public ResponseEntity<ApiResponse<String>> deleteItem(
+            @PathVariable Long id
     ) throws Exception {
         try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication.getPrincipal();
 
-        } catch (Exception e) {
+            if(principal instanceof UserDetails userDetails) {
+                Long userId = ((CustomUserDetails) userDetails).getUserId();
 
+                stockService.deleteItem(id, userId);
+
+                return ResponseEntity.status(HttpStatus.OK)
+                        .body(ApiResponse.success());
+            }
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(ApiResponse.success());
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(ApiResponse.fail(ErrorCode.BAD_REQUEST, "해당 종목을 찾을 수 없습니다."));
         }
     }
 }
