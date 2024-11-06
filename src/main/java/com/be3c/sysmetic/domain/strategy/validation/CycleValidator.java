479f764b1b85fadf783854f7cb4c0854f53702e9
한지선 <seon.hannn@gmail.com>
2024-11-06 18:01:43 +0900

    feat: entity, dto, repository 구현

--- src/main/java/com/be3c/sysmetic/domain/strategy/validation/CycleValidator.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/validation/CycleValidator.java
@@ -5,7 +5,6 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class CycleValidator implements ConstraintValidator<ValidCycle, Character> {
 
-    // 주기 D 또는 P가 아닐 경우 exception
     @Override
     public boolean isValid(Character value, ConstraintValidatorContext context) {
         return value == 'D' || value == 'P';
