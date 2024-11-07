00b5d9633deae46da4f125918bde3555fd8f9111
nardis0321 <nardis0321@gmail.com>
2024-11-07 11:16:59 +0900

    settings: BaseEntity와 관련 설정 및 MapStruct 의존성 추가 #32
    - JPA Auditing 기능을 활성화하여 생성/수정 시간 및 사용자 정보를 자동으로 추적
    - BaseEntity 클래스를 추가하여 공통 필드(createdAt, updatedAt, createdBy, updatedBy) 관리
    - MapStruct 의존성을 추가하여 DTO와 엔티티 간의 매핑을 효율적으로 처리할 수 있도록 설정

---
+++ src/main/java/com/be3c/sysmetic/global/entity/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.be3c.sysmetic.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
