307d5d7b65f63a26074d320531abf0c7fcf58b44
Eseas <8snlf9@gmail.com>
2024-11-07 11:17:54 +0900

    feat: 매매 유형 페이지 찾기 구현
    완료
    - 페이징으로 매매 유형 찾기
    추가 보안 사항
    - 아이콘 찾기 코드 추가 필요

--- src/main/java/com/be3c/sysmetic/domain/strategy/service/MethodService.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/service/MethodService.java
@@ -1,10 +1,13 @@
 package com.be3c.sysmetic.domain.strategy.service;
 
+import com.be3c.sysmetic.domain.strategy.dto.MethodGetResponseDto;
 import com.be3c.sysmetic.domain.strategy.entity.Method;
+import org.springframework.data.domain.Page;
 
 import java.util.Optional;
 
 public interface MethodService {
     boolean duplCheck(String name);
-    Method findById(Long id);
+    MethodGetResponseDto findById(Long id);
+    Page<MethodGetResponseDto> findMethodPage(Integer page);
 }
--- src/main/java/com/be3c/sysmetic/domain/strategy/service/MethodServiceImpl.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/service/MethodServiceImpl.java
@@ -1,12 +1,15 @@
 package com.be3c.sysmetic.domain.strategy.service;
 
+import com.be3c.sysmetic.domain.strategy.dto.MethodGetResponseDto;
 import com.be3c.sysmetic.domain.strategy.entity.Method;
 import com.be3c.sysmetic.domain.strategy.repository.MethodRepository;
 import com.be3c.sysmetic.global.common.Code;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,11 +21,26 @@ public class MethodServiceImpl implements MethodService {
 
     @Override
     public boolean duplCheck(String name) {
-        return methodRepository.findByName(name).isEmpty();
+        return methodRepository.findByNameAndStatusCode(name, Code.USING_STATE.getCode()).isEmpty();
     }
 
     @Override
-    public Method findById(Long id) throws NullPointerException {
+    public MethodGetResponseDto findById(Long id) throws NullPointerException {
         return methodRepository.findByIdAndStatusCode(id, Code.USING_STATE.getCode()).get();
+    }
+
+    /*
+        메서드 해당 페이지 데이터 반환
+        아이콘 찾기 메서드를 통해 해당 페이지에 넣기.
+     */
+
+    @Override
+    public Page<MethodGetResponseDto> findMethodPage(Integer page) {
+        Pageable pageable = PageRequest.of(page, 10, Sort.by("createdDate").descending());
+        Page<MethodGetResponseDto> find_page = methodRepository.findAllByStatusCode(pageable, Code.USING_STATE.getCode());
+
+//        파일 패스 찾는 메서드 추가 예정
+//        find_page.getContent().get(0).setFile_path();
+        return find_page;
     }
 }
