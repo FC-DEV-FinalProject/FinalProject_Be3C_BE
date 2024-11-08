bed2122a741585bb868c1e3b91d36a08752eae17
mminn20 <mminnkk20@gmail.com>
2024-11-08 15:01:41 +0900

    feat : 전략 목록 조회 테스트 추가 #8

--- src/main/java/com/be3c/sysmetic/domain/strategy/entity/Strategy.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/entity/Strategy.java
@@ -1,7 +1,9 @@
 package com.be3c.sysmetic.domain.strategy.entity;
 
 import com.be3c.sysmetic.domain.member.entity.Member;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -55,7 +57,8 @@ public class Strategy {
     private Double smScore;
 
     // 누적수익률 추가
-    @Column(name = "accum_profit_rate", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "#00.00")
+    @Column(name = "accum_profit_rate", nullable = true)
     private Double accumProfitRate;
 
     @Column(name = "strategy_created_date", nullable = false)
