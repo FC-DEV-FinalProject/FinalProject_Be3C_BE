6e564335fdf54a5d1eb8ed1f3ea6c8ced1057695
한지선 <seon.hannn@gmail.com>
2024-11-08 18:13:43 +0900

    feat: 전략 수정 #9

--- src/main/java/com/be3c/sysmetic/domain/strategy/repository/StrategyStockReferenceRepository.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/repository/StrategyStockReferenceRepository.java
@@ -2,11 +2,25 @@ package com.be3c.sysmetic.domain.strategy.repository;
 
 import com.be3c.sysmetic.domain.strategy.entity.StrategyStockReference;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface StrategyStockReferenceRepository extends JpaRepository<StrategyStockReference, Long> {
     List<StrategyStockReference> findByStrategyId(Long strategyId);
+
+    @Query("SELECT sr.stock.id FROM StrategyStockReference sr WHERE sr.strategy.id = :strategyId")
+    List<Long> findStockIdsByStrategyId(Long strategyId);
+
+    void deleteByStrategyId(Long strategyId);
+
+    void deleteByStrategyIdAndStockId(Long strategyId, Long stockId);
+
+    @Modifying
+    @Query("DELETE FROM StrategyStockReference sr WHERE sr.strategy.id = :strategyId AND sr.stock.id IN :stockIds")
+    void deleteByStrategyIdAndStockIds(Long strategyId, Set<Long> stockIds);
 }
