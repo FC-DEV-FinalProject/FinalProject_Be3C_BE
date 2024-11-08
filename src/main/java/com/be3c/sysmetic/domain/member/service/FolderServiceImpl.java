210682f99e2364cb1badad46725152d771bc7c84
Eseas <8snlf9@gmail.com>
2024-11-08 12:06:03 +0900

    feat: 폴더명 중복 체크
    완료
    - 폴더명 중복 체크 api 구현

--- src/main/java/com/be3c/sysmetic/domain/member/service/FolderServiceImpl.java
+++ src/main/java/com/be3c/sysmetic/domain/member/service/FolderServiceImpl.java
@@ -6,9 +6,12 @@ import com.be3c.sysmetic.global.common.Code;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -18,12 +21,29 @@ public class FolderServiceImpl implements FolderService {
 
     @Override
     public List<Folder> getUserFolders(Long id) {
-        List<Folder> folderList = folderRepository.findByIdAndStatusCode(id, Code.USING_STATE.getCode());
+        List<Folder> folderList = folderRepository
+                .findByMemberIdAndStatusCode(id, Code.USING_STATE.getCode());
 
         if(folderList == null || folderList.isEmpty()) {
             throw new EntityNotFoundException();
         }
 
         return folderList;
+    }
+
+    @Override
+    public boolean duplCheck(Long id, String name) {
+        Optional<Folder> folder = folderRepository
+                .findByMemberIdAndFolderNameAndStatusCode(
+                        id,
+                        name,
+                        Code.USING_STATE.getCode()
+                );
+
+        if(folder.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "폴더명이 중복되었습니다.");
+        }
+
+        return true;
     }
 }
