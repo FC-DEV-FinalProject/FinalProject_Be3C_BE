fe03a87ff81aa3f6e68fb22e5333b04426dbaf37
mminn20 <mminnkk20@gmail.com>
2024-11-11 16:09:33 +0900

    feat : 트레이더 닉네임으로 검색 - 테스트 #8

--- src/test/java/com/be3c/sysmetic/domain/strategy/service/StrategyListServiceTest.java
+++ src/test/java/com/be3c/sysmetic/domain/strategy/service/StrategyListServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
@@ -42,6 +43,8 @@ public class StrategyListServiceTest {
     MethodRepository methodRepository;
     @Autowired
     EntityManager em;
+    @Autowired
+    private ServerProperties serverProperties;
 
     @BeforeEach
     public void init() {
@@ -313,6 +316,102 @@ public class StrategyListServiceTest {
             assertTrue(t.getFollowerCount() <= maxFollowerCount);
             System.out.println("id = " + t.getTraderId() + ", followerCount() = " + t.getFollowerCount());
         }
+    }
+
+    @Test
+    @DisplayName("특정 닉네임을 가진 트레이더 목록 전부 조회")
+    @Transactional
+    @Rollback(value = false)
+    public void findAllByTraderNickname() {
+        // before : 현재 데이터베이스 비우기
+        strategyListRepository.deleteAll();
+        assertTrue(strategyListRepository.findAll().isEmpty());
+
+        // 전략 수 난수는 [1, 100]
+        int randomStrategyNum = (int) (Math.random() * 100 + 1);
+        // int randomStrategyNum = 9;
+        System.out.println("randomStrategyNum = " + randomStrategyNum);
+
+        // 트레이더 저장
+        saveMember("여의도최고부자");
+        saveMember("여의도전략가");
+
+        // 트레이더 수만큼 전략 생성 후 저장
+        for (int i=0; i < randomStrategyNum; i++) {
+            Strategy s = Strategy.builder()
+                    .trader(i % 2 == 0 ? getTrader("여의도최고부자") : getTrader("여의도전략가"))
+                    .method(getMethod())
+                    .statusCode("ST001")
+                    .name("전략" + (i+1))
+                    .cycle('P')
+                    .minOperationAmount(100.0)
+                    .content("전략" + (i + 1) + " 소개 내용")
+                    .followerCount((long) (Math.random() * 100))
+                    .accumProfitRate(Math.random() * 100)
+                    .createdBy(1L)
+                    .modifiedBy(1L)
+                    .build();
+            em.persist(s);
+            em.flush();
+            em.clear();
+        }
+
+        // 전체 페이지 계산
+        int expectedTotalPage = (int) (Math.ceil(randomStrategyNum / 10.0));
+        int actualTotalPage = (int) strategyListService.getTotalPageNumber("ST001", 10);
+        assertEquals(expectedTotalPage, actualTotalPage);
+
+        // 전체 페이지 조회
+        for (int i=0; i < expectedTotalPage; i++) {
+            Page<TraderListDto> page = strategyListService.findByTrader("여의도");
+            assertEquals(page.getSize(), 10);
+            assertNotNull(page);
+            assertTrue(page.getSort().isSorted());
+
+            long maxFollowerCount = page.getContent().get(0).getFollowerCount();
+
+            for (int j=0; j < page.getContent().size(); j++) {
+                assertTrue(page.getContent().get(i).getFollowerCount() <= maxFollowerCount);
+                System.out.println("닉네임 = " + page.getContent().get(j).getNickname() + ", followerCount = " + page.getContent().get(j).getFollowerCount());
+            }
+            System.out.println("=====" + i + "======");
+        }
+
+    }
+
+
+
+    @Test
+    @DisplayName("특정 닉네임을 가진 트레이더 없음")
+    @Transactional
+    @Rollback(value = false)
+    public void failToFindTrader() {
+        // before : 현재 데이터베이스 비우기
+        strategyListRepository.deleteAll();
+        assertTrue(strategyListRepository.findAll().isEmpty());
+
+        saveMember("10층에도살아요");
+
+        Strategy s = Strategy.builder()
+                .trader(getTrader("10층에도살아요"))
+                .method(getMethod())
+                .statusCode("ST001")
+                .name("전략" + ("김삼성"))
+                .cycle('P')
+                .minOperationAmount(100.0)
+                .content("전략 소개 내용")
+                .followerCount((long) (Math.random() * 100))
+                .accumProfitRate(Math.random() * 100)
+                .createdBy(1L)
+                .modifiedBy(1L)
+                .build();
+        em.persist(s);
+        em.flush();
+        em.clear();
+
+
+        Page<TraderListDto> page = strategyListService.findByTrader("강남");
+        assertFalse(page.hasContent());
     }
 
 
