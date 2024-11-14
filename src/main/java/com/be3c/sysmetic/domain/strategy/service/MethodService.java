12b0352b9592186877d35a914847ea6a91ad8e77
Eseas <8snlf9@gmail.com>
2024-11-14 16:33:20 +0900

    feat : PageResponseDto > PageResponse로 변경
    완료
    - PageResponse 적용

--- src/main/java/com/be3c/sysmetic/domain/strategy/service/MethodService.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/service/MethodService.java
@@ -3,16 +3,12 @@ package com.be3c.sysmetic.domain.strategy.service;
 import com.be3c.sysmetic.domain.strategy.dto.MethodGetResponseDto;
 import com.be3c.sysmetic.domain.strategy.dto.MethodPostRequestDto;
 import com.be3c.sysmetic.domain.strategy.dto.MethodPutRequestDto;
-import com.be3c.sysmetic.domain.strategy.entity.Method;
-import com.be3c.sysmetic.global.common.response.PageResponseDto;
-import org.springframework.data.domain.Page;
-
-import java.util.Optional;
+import com.be3c.sysmetic.global.common.response.PageResponse;
 
 public interface MethodService {
     boolean duplCheck(String name);
     MethodGetResponseDto findById(Long id);
-    PageResponseDto<MethodGetResponseDto> findMethodPage(Integer page);
+    PageResponse<MethodGetResponseDto> findMethodPage(Integer page);
 
     boolean insertMethod(MethodPostRequestDto methodPostRequestDto);
     boolean updateMethod(MethodPutRequestDto methodPutRequestDto);
