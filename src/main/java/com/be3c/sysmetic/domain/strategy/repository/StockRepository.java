073aa1b07dc24fd31c092ebff99492ffbc69ebe4
Eseas <8snlf9@gmail.com>
2024-11-14 10:30:25 +0900

    feat: 완료
    완료
    - 기본 구현 완료
    추가 개발 요구 사항
    - S3 아이콘 업로드 및 찾기 기능 추가 필요

--- src/main/java/com/be3c/sysmetic/domain/strategy/repository/StockRepository.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/repository/StockRepository.java
@@ -1,9 +1,11 @@
 package com.be3c.sysmetic.domain.strategy.repository;
 
+import com.be3c.sysmetic.domain.strategy.dto.StockGetResponseDto;
 import com.be3c.sysmetic.domain.strategy.entity.Stock;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,5 +17,12 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
     Optional<Stock> findByName(String name);
     Optional<Stock> findByNameAndStatusCode(String name, String statusCode);
 
-    Page<Stock> findAllByStatusCode(Pageable pageable, String statusCode);
+    @Query("""
+        SELECT new com.be3c.sysmetic.domain.strategy.dto.StockGetResponseDto(
+            s.id, s.name, null
+        )
+        FROM Stock s
+        WHERE s.statusCode = :statusCode
+    """)
+    Page<StockGetResponseDto> findAllByStatusCode(String statusCode, Pageable pageable);
 }
--- src/main/java/com/be3c/sysmetic/domain/strategy/service/StockService.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/service/StockService.java
@@ -1,15 +1,14 @@
 package com.be3c.sysmetic.domain.strategy.service;
 
+import com.be3c.sysmetic.domain.strategy.dto.StockGetResponseDto;
 import com.be3c.sysmetic.domain.strategy.dto.StockPostRequestDto;
 import com.be3c.sysmetic.domain.strategy.dto.StockPutRequestDto;
-import com.be3c.sysmetic.domain.strategy.entity.Stock;
-import org.springframework.data.domain.Page;
+import com.be3c.sysmetic.global.common.response.PageResponse;
 
 public interface StockService {
-    Stock findItemById(Long id);
+    StockGetResponseDto findItemById(Long id);
     boolean duplcheck(String name);
-    Object findItemIcon(Long itemId);
-    Page<Stock> findItemPage(Integer page);
+    PageResponse<StockGetResponseDto> findItemPage(Integer page);
 
     boolean saveItem(StockPostRequestDto requestDto);
     boolean updateItem(StockPutRequestDto requestDto);
