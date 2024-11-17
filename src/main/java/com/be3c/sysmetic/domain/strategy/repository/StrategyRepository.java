986e3a296525984673102808c546b5ed2bc56f46
한지선 <seon.hannn@gmail.com>
2024-11-17 14:45:07 +0900

    feat: 전략 통계 계산, 조회 service 구현 #48

--- src/main/java/com/be3c/sysmetic/domain/strategy/repository/StrategyRepository.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/repository/StrategyRepository.java
@@ -2,9 +2,15 @@ package com.be3c.sysmetic.domain.strategy.repository;
 
 import com.be3c.sysmetic.domain.strategy.entity.Strategy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StrategyRepository extends JpaRepository<Strategy, Long> {
     boolean existsByName(String name); // 전략명 중복 확인
+
+    @Query("SELECT s FROM Strategy s WHERE s.statusCode = 'PUBLIC'")
+    List<Strategy> findAllByPublicStatus();
 }
