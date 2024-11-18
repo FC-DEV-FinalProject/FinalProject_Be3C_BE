f07d2e45b1befaa72ba80910a9a5e3acbaf20cbb
한지선 <seon.hannn@gmail.com>
2024-11-18 17:08:07 +0900

    feat: controller swagger 적용
    전략 등록, 월간분석 조회, 일간분석 조회 스웨거 적용

--- src/main/java/com/be3c/sysmetic/domain/strategy/controller/TraderStrategyController.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/controller/TraderStrategyController.java
@@ -1,5 +1,6 @@
 package com.be3c.sysmetic.domain.strategy.controller;
 
+import com.be3c.sysmetic.domain.strategy.dto.DailyResponseDto;
 import com.be3c.sysmetic.domain.strategy.dto.SaveDailyRequestDto;
 import com.be3c.sysmetic.domain.strategy.dto.SaveDailyResponseDto;
 import com.be3c.sysmetic.domain.strategy.dto.SaveStrategyRequestDto;
@@ -8,7 +9,13 @@ import com.be3c.sysmetic.domain.strategy.service.DailyServiceImpl;
 import com.be3c.sysmetic.domain.strategy.service.DeleteStrategyServiceImpl;
 import com.be3c.sysmetic.domain.strategy.service.UpdateStrategyServiceImpl;
 import com.be3c.sysmetic.domain.strategy.service.InsertStrategyServiceImpl;
-import com.be3c.sysmetic.global.common.response.ApiResponse;
+import com.be3c.sysmetic.global.common.response.APIResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +23,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+@Tag(name = "트레이더 전략 API", description = "트레이더 전략 API")
 @RequestMapping("/trader")
 @RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
 @RestController
@@ -30,64 +37,75 @@ public class TraderStrategyController {
     private final DailyServiceImpl dailyService;
 
     // 전략 등록
+    @Operation(summary = "전략 등록", description = "트레이더가 자신의 전략을 등록하는 API")
+    @ApiResponses(
+            {
+                    @ApiResponse(responseCode = "200", description = "등록 요청 성공",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(responseCode = "401", description = "권한 없음으로 인한 실패",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @PostMapping("/strategy")
-    public ResponseEntity<ApiResponse<Strategy>> insertStrategy(@Valid @RequestBody SaveStrategyRequestDto requestDto) {
+    public ResponseEntity<APIResponse<Strategy>> insertStrategy(@Valid @RequestBody SaveStrategyRequestDto requestDto) {
         insertStrategyService.insertStrategy(requestDto);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success());
+                .body(APIResponse.success());
     }
 
     // 전략명 중복
     @GetMapping("/strategy/duplication-name")
-    public ResponseEntity<ApiResponse<Boolean>> checkDuplicationStrategyName(@RequestParam String name) {
+    public ResponseEntity<APIResponse<Boolean>> checkDuplicationStrategyName(@RequestParam String name) {
         boolean isDuplication = insertStrategyService.returnIsDuplicationName(name);
         // 중복 true, 미중복 false 반환
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(isDuplication));
+                .body(APIResponse.success(isDuplication));
     }
 
     // 전략 수정
     @PatchMapping("/strategy")
-    public ResponseEntity<ApiResponse<Strategy>> updateStrategy(@RequestParam Long strategyId, @Valid @RequestBody SaveStrategyRequestDto requestDto) {
+    public ResponseEntity<APIResponse<Strategy>> updateStrategy(@RequestParam Long strategyId, @Valid @RequestBody SaveStrategyRequestDto requestDto) {
         updateStrategyService.updateStrategy(strategyId, requestDto);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success());
+                .body(APIResponse.success());
     }
 
     // 전략 삭제
     @DeleteMapping("/strategy")
-    public ResponseEntity<ApiResponse> deleteStrategy(@RequestParam Long strategyId) {
+    public ResponseEntity<APIResponse> deleteStrategy(@RequestParam Long strategyId) {
         deleteStrategyService.deleteStrategy(strategyId);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success());
+                .body(APIResponse.success());
     }
 
     // 일간데이터 등록
     @PostMapping("/strategy/daily")
-    public ResponseEntity<ApiResponse> insertDaily(@RequestParam("strategyId") Long strategyId, @Valid @RequestBody List<SaveDailyRequestDto> requestDtoList) {
+    public ResponseEntity<APIResponse> insertDaily(@RequestParam("strategyId") Long strategyId, @Valid @RequestBody List<SaveDailyRequestDto> requestDtoList) {
         SaveDailyResponseDto responseDto = dailyService.getIsDuplicate(strategyId, requestDtoList);
         dailyService.saveDaily(strategyId, requestDtoList);
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success(responseDto));
+                .body(APIResponse.success(responseDto));
     }
 
     // 일간데이터 수정
     @PatchMapping("/strategy/daily")
-    public ResponseEntity<ApiResponse> updateDaily(@RequestParam("strategyId") Long strategyId, @RequestParam("dailyId") Long dailyId, @RequestBody SaveDailyRequestDto requestDto) {
+    public ResponseEntity<APIResponse> updateDaily(@RequestParam("strategyId") Long strategyId, @RequestParam("dailyId") Long dailyId, @RequestBody SaveDailyRequestDto requestDto) {
         dailyService.updateDaily(strategyId, dailyId, requestDto);
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success());
+                .body(APIResponse.success());
     }
 
     // 일간데이터 삭제
     @DeleteMapping("/strategy/daily")
-    public ResponseEntity<ApiResponse> deleteDaily(@RequestParam("strategyId") Long strategyId, @RequestParam("dailyId") Long dailyId) {
+    public ResponseEntity<APIResponse> deleteDaily(@RequestParam("strategyId") Long strategyId, @RequestParam("dailyId") Long dailyId) {
         dailyService.deleteDaily(strategyId, dailyId);
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.success());
+                .body(APIResponse.success());
     }
 
 }
