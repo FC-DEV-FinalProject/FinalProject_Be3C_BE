69b826abffb0c27779a99cdda92d0f1548f7c84e
Eseas <8snlf9@gmail.com>
2024-11-18 19:36:04 +0900

    test: 젠킨스 11차 테스트
    완료

--- src/test/java/com/be3c/sysmetic/domain/strategy/repository/UpdateStrategyRepositoryTest.java
+++ src/test/java/com/be3c/sysmetic/domain/strategy/repository/UpdateStrategyRepositoryTest.java
@@ -42,6 +42,7 @@ public class UpdateStrategyRepositoryTest {
         saveStock();
 
         strategyRepository.deleteAll();
+
         strategyRepository.save(getStrategy(getRequestDto(), findMember(), findMethod()));
     }
 
