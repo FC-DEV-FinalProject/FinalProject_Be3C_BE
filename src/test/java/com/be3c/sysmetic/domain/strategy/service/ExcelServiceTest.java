882be10c9482c7aab65a6e441dd2e616d0914540
nardis0321 <nardis0321@gmail.com>
2024-11-20 13:31:02 +0900

    test: 엔티티 구조에 맞춰 테스트 코드 업데이트 #4
    - not nullable 필드를 포함하여 객체 초기화하는 방식으로 테스트 코드 수정

--- src/test/java/com/be3c/sysmetic/domain/strategy/service/ExcelServiceTest.java
+++ src/test/java/com/be3c/sysmetic/domain/strategy/service/ExcelServiceTest.java
@@ -1,6 +1,11 @@
 package com.be3c.sysmetic.domain.strategy.service;
 
+import com.be3c.sysmetic.domain.member.entity.Member;
+import com.be3c.sysmetic.domain.member.repository.MemberRepository;
+import com.be3c.sysmetic.domain.strategy.entity.Method;
 import com.be3c.sysmetic.domain.strategy.entity.Strategy;
+import com.be3c.sysmetic.domain.strategy.repository.MethodRepository;
+import com.be3c.sysmetic.domain.strategy.repository.StrategyRepository;
 import com.be3c.sysmetic.domain.strategy.util.StrategyCalculator;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -10,40 +15,90 @@ import com.be3c.sysmetic.domain.strategy.util.DoubleHandler;
 import com.be3c.sysmetic.global.util.file.exception.InvalidFileFormatException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@Transactional
+@SpringBootTest
 public class ExcelServiceTest {
 
     private ExcelService excelService;
 
-    @Mock
+    @Autowired
     private DailyRepository dailyRepository;
 
-    @Mock
+    @Autowired
+    private StrategyRepository strategyRepository;
+
+    @Autowired
+    private MethodRepository methodRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
     private DoubleHandler doubleHandler;
 
-    @Mock
+    @Autowired
     private StrategyCalculator strategyCalculator;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
-        excelService = new ExcelServiceImpl(dailyRepository, doubleHandler, strategyCalculator);
+        excelService = new ExcelServiceImpl(dailyRepository, strategyRepository,  doubleHandler, strategyCalculator);
+
+        // Member 객체 초기화
+        Member member = Member.builder()
+                .roleCode("USER")
+                .email("testuser@example.com")
+                .password("password123")
+                .name("Test User")
+                .nickname("testuser")
+                .phoneNumber("010-1234-5678")
+                .usingStatusCode("ACTIVE")
+                .totalFollow(100)
+                .totalStrategyCount(5)
+                .receiveInfoConsent("Y")
+                .infoConsentDate(LocalDateTime.now())
+                .receiveMarketingConsent("Y")
+                .marketingConsentDate(LocalDateTime.now())
+                .build();
+        memberRepository.save(member);
+
+        // Method 객체 초기화
+        Method method = Method.builder()
+                .name("Test Method")
+                .statusCode("ACTIVE")
+                .methodCreatedDate(LocalDateTime.now())
+                .build();
+        methodRepository.save(method);
+
+        // Strategy 객체 초기화
+        Strategy strategy = Strategy.builder()
+                .trader(member)  // Member 객체 설정
+                .method(method)   // Method 객체 설정
+                .statusCode("ACTIVE")
+                .name("Sample Strategy")
+                .cycle('D')
+                .content("This is a sample strategy content")
+                .followerCount(0L)
+                .kpRatio(0.0)
+                .smScore(0.0)
+                .strategyCreatedDate(LocalDateTime.now())
+                .strategyModifiedDate(LocalDateTime.now())
+                .build();
+        strategyRepository.save(strategy);
+
     }
 
 
@@ -119,39 +174,27 @@ public class ExcelServiceTest {
         // MockMultipartFile로 변환
         MockMultipartFile file = new MockMultipartFile("file", "test.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", content);
 
-        // 2. Mock repository behavior
-        Long strategyId = 1L;   // 참조할 전략id
+        Strategy strategy = strategyRepository.findAll().get(0);
+        Long strategyId = strategy.getId();   // 참조할 전략id
 
         Daily oldEntity1 = Daily.builder()
-                .id(1L)
-                .strategy(Strategy.builder()
-                        .id(strategyId).build())
+                .strategy(strategy)
                 .date(LocalDate.of(2024, 11, 12))
                 .depositWithdrawalAmount(1000.0)
                 .profitLossAmount(200.0)
                 .build();
 
         Daily oldEntity2 = Daily.builder()
-                .id(2L)
-                .strategy(Strategy.builder()
-                        .id(strategyId).build())
+                .strategy(strategy)
                 .date(LocalDate.of(2024, 11, 13))
                 .depositWithdrawalAmount(2000.0)
                 .profitLossAmount(400.0)
                 .build();
 
-        when(dailyRepository.findAll()).thenReturn(List.of(oldEntity1, oldEntity2));
-
-        // 3. Mock doubleHandler behavior
-//        when(doubleHandler.cutDouble(anyDouble())).thenAnswer(invocation -> invocation.getArgument(0));
-
         // 4. Call the service method
         excelService.uploadExcel(file, strategyId);
 
         System.out.println("dailyRepository.findAll() = " + dailyRepository.findAll());
-
-        // 5. Verify results and interactions
-        verify(dailyRepository, times(1)).saveAll(anyList());  // saveAll이 몇 번 호출되었는지 검증
 
         List<Daily> result = dailyRepository.findAll();
         assertNotNull(result);
@@ -160,11 +203,12 @@ public class ExcelServiceTest {
 
     @Test // 잘못된 파일 형식으로 테스트
     void testUploadExcel_invalidFileFormat() {
+        Strategy strategy = strategyRepository.findAll().get(0);
 
         MockMultipartFile invalidFile = new MockMultipartFile("file", "invalid.txt", "text/plain", "Invalid file content".getBytes());
 
         Exception exception = assertThrows(InvalidFileFormatException.class, () -> {
-            excelService.uploadExcel(invalidFile, 1L);
+            excelService.uploadExcel(invalidFile, strategy.getId());
         });
 
         assertEquals("엑셀 업로드 실패: 파일 형식이 올바르지 않습니다.", exception.getMessage());
@@ -172,11 +216,12 @@ public class ExcelServiceTest {
 
     @Test // 빈 파일을 업로드할 경우
     void testUploadExcel_emptyFile() {
+        Strategy strategy = strategyRepository.findAll().get(0);
 
         MockMultipartFile emptyFile = new MockMultipartFile("file", "empty.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", new byte[0]);
 
         Exception exception = assertThrows(InvalidFileFormatException.class, () -> {
-            excelService.uploadExcel(emptyFile,1L);
+            excelService.uploadExcel(emptyFile,strategy.getId());
         });
 
         assertEquals("엑셀 업로드 실패: 파일 내용이 올바르지 않습니다.", exception.getMessage());
@@ -195,34 +240,40 @@ public class ExcelServiceTest {
 
     @Test
     public void testDownloadExcelWithStatistics() throws Exception {
+        Strategy strategy = strategyRepository.findAll().get(0);
+
         // 1. 테스트 데이터 준비
-        Daily daily1 = new Daily();
-        daily1.setStrategy(Strategy.builder().id(1L).build());
-        daily1.setDate(LocalDate.parse("2024-01-01"));
-        daily1.setPrincipal(1000.0);
-        daily1.setDepositWithdrawalAmount(500.0);
-        daily1.setProfitLossAmount(50.0);
-        daily1.setProfitLossRate(5.0);
-        daily1.setAccumulatedProfitLossAmount(50.0);
-        daily1.setAccumulatedProfitLossRate(5.0);
+        Daily daily1 = Daily.builder()
+                .strategy(strategy)
+                .date(LocalDate.parse("2024-01-01"))
+                .principal(1000.0)
+                .currentBalance(1000.0)
+                .standardAmount(1000.0)
+                .depositWithdrawalAmount(500.0)
+                .profitLossAmount(50.0)
+                .profitLossRate(5.0)
+                .accumulatedProfitLossAmount(50.0)
+                .accumulatedProfitLossRate(5.0)
+                .build();
 
-        Daily daily2 = new Daily();
-        daily2.setStrategy(Strategy.builder().id(1L).build());
-        daily2.setDate(LocalDate.parse("2024-01-02"));
-        daily2.setPrincipal(2000.0);
-        daily2.setDepositWithdrawalAmount((double) -300);
-        daily2.setProfitLossAmount((double) -30);
-        daily2.setProfitLossRate(-1.5);
-        daily2.setAccumulatedProfitLossAmount(20.0);
-        daily2.setAccumulatedProfitLossRate(1.0);
+        Daily daily2 = Daily.builder()
+                .strategy(strategy)
+                .date(LocalDate.parse("2024-01-02"))
+                .principal(2000.0)
+                .currentBalance(2000.0)
+                .standardAmount(2000.0)
+                .depositWithdrawalAmount(-300.0)
+                .profitLossAmount(-30.0)
+                .profitLossRate(-1.5)
+                .accumulatedProfitLossAmount(20.0)
+                .accumulatedProfitLossRate(1.0)
+                .build();
 
-        List<Daily> mockDailyList = Arrays.asList(daily1, daily2);
-
-        // 2. mock 객체 설정 (findAll 메서드가 호출되면 mock 데이터를 반환)
-        when(dailyRepository.findAll()).thenReturn(mockDailyList);
+        List<Daily> dailyList = Arrays.asList(daily1, daily2);
+        dailyRepository.saveAll(dailyList);
 
         // 3. 메서드 실행
-        InputStream inputStream = excelService.downloadExcelWithStatistics(1L);
+        InputStream inputStream = excelService.downloadExcelWithStatistics(strategy.getId());
 
         // 4. 엑셀 파일 검증
         assertNotNull(inputStream, "엑셀 파일이 null이어서는 안 됩니다.");
@@ -262,8 +313,5 @@ public class ExcelServiceTest {
             assertEquals(20, dataRow2.getCell(5).getNumericCellValue(), 0.0);
             assertEquals(1.0, dataRow2.getCell(6).getNumericCellValue(), 0.0);
         }
-
-        // 5. verify 메서드를 통해 dailyRepository.findAll()이 한 번만 호출되었는지 확인
-        verify(dailyRepository, times(1)).findAll();
     }
 }
