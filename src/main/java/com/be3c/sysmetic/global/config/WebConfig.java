bb18a67087cf63b21d251429b5bee08b0288ffa4
KJU3 <kju_3@naver.com>
2024-11-21 16:09:51 +0900

    settings: Netlify WebCongig file add #65

---
+++ src/main/java/com/be3c/sysmetic/global/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.be3c.sysmetic.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:63342",
+                        "http://localhost:8080",
+                        "https://deploy-preview-103--sysmetic-be3c.netlify.app",
+                        "https://3.39.211.122.nip.io"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
