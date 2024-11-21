ef587b132eab88a93242a965a8f7bfd1c15aea5a
KJU3 <kju_3@naver.com>
2024-11-21 11:42:52 +0900

    settings: Netlify Test Controller add #65

---
+++ src/main/java/com/be3c/sysmetic/domain/TestController.java
@@ -0,0 +1,18 @@
+package com.be3c.sysmetic.domain;
+
+
+import com.be3c.sysmetic.global.common.response.APIResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public ResponseEntity<APIResponse<String >> testMethod(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(APIResponse.success("Backend Server Communication Success!"));
+    }
+}
