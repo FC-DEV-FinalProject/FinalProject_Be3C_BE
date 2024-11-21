49afd8f07420125c6870ebec0c537f95ce54bed3
KJU3 <kju_3@naver.com>
2024-11-21 21:19:42 +0900

    settings: CORS Setting modify #65

--- src/main/java/com/be3c/sysmetic/global/config/WebConfig.java
+++ src/main/java/com/be3c/sysmetic/global/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("https://3.39.211.122.nip.io", "http://3.39.211.122.nip.io", "*") // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("Authorization", "Content-Type") // 허용할 헤더
-                .allowCredentials(true); // 인증 정보 포함 허용
+                .allowedOrigins("*") // 허용할 출처
+                .allowedMethods("*") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(false); // 인증 정보 포함 허용
     }
 }
