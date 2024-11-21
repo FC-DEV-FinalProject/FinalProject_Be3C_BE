192fd765f39c9f84ca065c97f3e469226723d2e6
KJU3 <kju_3@naver.com>
2024-11-21 21:12:12 +0900

    settings: CORS Setting add #65

--- src/main/java/com/be3c/sysmetic/global/config/WebConfig.java
+++ src/main/java/com/be3c/sysmetic/global/config/WebConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("https://3.39.211.122.nip.io") // 허용할 출처
+                .allowedOrigins("https://3.39.211.122.nip.io", "http://3.39.211.122.nip.io", "*") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("Authorization", "Content-Type") // 허용할 헤더
                 .allowCredentials(true); // 인증 정보 포함 허용
