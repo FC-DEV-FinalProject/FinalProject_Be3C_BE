4826a212c11cc66b53db031cdcc72b146b4441b9
hekim52 <acrossnightsky@gmail.com>
2024-11-25 18:28:46 +0900

    update: 공지사항 Dto 수정 #87

--- src/main/java/com/be3c/sysmetic/domain/member/dto/NoticeDetailAdminShowResponseDto.java
+++ src/main/java/com/be3c/sysmetic/domain/member/dto/NoticeDetailAdminShowResponseDto.java
@@ -3,6 +3,7 @@ package com.be3c.sysmetic.domain.member.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -39,4 +40,16 @@ public class NoticeDetailAdminShowResponseDto {
 
     @Schema(description = "공개 여부 (0: 비공개, 1: 공개)", example = "1")
     private Integer isOpen;
+
+    @Schema(description = "이전글 제목", example = "공지사항 제목 예")
+    private String previousTitle;
+
+    @Schema(description = "이전글 작성일시", example = "2023-11-21T10:15:30")
+    private LocalDateTime previousWriteDate;
+
+    @Schema(description = "다음글 제목", example = "공지사항 제목 예")
+    private String nextTitle;
+
+    @Schema(description = "다음글 작성일시", example = "2023-11-21T10:15:30")
+    private LocalDateTime nextWriteDate;
 }
