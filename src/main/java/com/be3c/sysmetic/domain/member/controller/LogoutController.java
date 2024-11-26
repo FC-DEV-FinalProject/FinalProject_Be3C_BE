35c453bdf0b90f744f547b0a4b23823dc1f6ee83
jeongns2611 <jeongns2611@gmail.com>
2024-11-26 12:34:20 +0900

    chore: swagger setting - member version 추가

--- src/main/java/com/be3c/sysmetic/domain/member/controller/LogoutController.java
+++ src/main/java/com/be3c/sysmetic/domain/member/controller/LogoutController.java
@@ -13,11 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "로그아웃 API", description = "로그아웃")
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequestMapping("/v1")
 public class LogoutController {
 
     private final LogoutService logoutService;
