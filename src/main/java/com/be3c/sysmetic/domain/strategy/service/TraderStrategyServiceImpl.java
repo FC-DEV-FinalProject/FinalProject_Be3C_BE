fd68ec32eed2d195777c9a1cd097cc34bdaacc01
한지선 <seon.hannn@gmail.com>
2024-11-28 17:00:24 +0900

    fix: 전략 등록시 default 값 추가

--- src/main/java/com/be3c/sysmetic/domain/strategy/service/TraderStrategyServiceImpl.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/service/TraderStrategyServiceImpl.java
@@ -53,6 +53,11 @@ public class TraderStrategyServiceImpl implements TraderStrategyService {
                 .name(requestDto.getName())
                 .cycle(requestDto.getCycle())
                 .content(requestDto.getContent())
+                .followerCount(0L)
+                .kpRatio(0.0)
+                .smScore(0.0)
+                .mdd(0.0)
+                .accumulatedProfitLossRate(0.0)
                 .build();
 
         strategyRepository.save(saveStrategy);
