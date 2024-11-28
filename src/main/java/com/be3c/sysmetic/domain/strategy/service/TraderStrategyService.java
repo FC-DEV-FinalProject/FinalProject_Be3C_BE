5ca5b0c5ac60705b9929425e819fadc6833583e1
한지선 <seon.hannn@gmail.com>
2024-11-28 18:16:32 +0900

    update: 전략 등록시 저장된 전략id 반환

--- src/main/java/com/be3c/sysmetic/domain/strategy/service/TraderStrategyService.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/service/TraderStrategyService.java
@@ -1,10 +1,11 @@
 package com.be3c.sysmetic.domain.strategy.service;
 
 import com.be3c.sysmetic.domain.strategy.dto.StrategyPostRequestDto;
+import com.be3c.sysmetic.domain.strategy.dto.StrategyPostResponseDto;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface TraderStrategyService {
-    void insertStrategy(StrategyPostRequestDto requestDto, MultipartFile file);
+    StrategyPostResponseDto insertStrategy(StrategyPostRequestDto requestDto, MultipartFile file);
     void updateStrategy(Long strategyId, StrategyPostRequestDto requestDto, MultipartFile file);
     void deleteStrategy(Long strategyId);
 }
