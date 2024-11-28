e9cc5dc9a1d6b4c441f1e77274f97ebb628942cc
Eseas <8snlf9@gmail.com>
2024-11-28 10:31:25 +0900

    fix: 매핑 오류 해결
    완료
    - @JsonProperty 명시

--- src/main/java/com/be3c/sysmetic/domain/member/dto/FolderGetRequestDto.java
+++ src/main/java/com/be3c/sysmetic/domain/member/dto/FolderGetRequestDto.java
@@ -1,6 +1,5 @@
 package com.be3c.sysmetic.domain.member.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
@@ -10,11 +9,14 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "폴더 조회 요청 DTO", example = "{ \"folderId\": 123, \"page\": 1 }")
 public class FolderGetRequestDto {
 
-    @NotNull
+    @Schema(description = "폴더 ID", example = "123")
+    @NotNull(message = "폴더 ID는 필수 값입니다.")
     private Long folderId;
 
-    @NotNull
+    @Schema(description = "페이지 번호", example = "1")
+    @NotNull(message = "페이지 번호는 필수 값입니다.")
     private Integer page;
 }
--- src/main/java/com/be3c/sysmetic/domain/member/dto/FollowPostRequestDto.java
+++ src/main/java/com/be3c/sysmetic/domain/member/dto/FollowPostRequestDto.java
@@ -1,5 +1,6 @@
 package com.be3c.sysmetic.domain.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,9 +16,11 @@ public class FollowPostRequestDto {
 
     @NotNull
     @Schema(description = "폴더 ID", example = "1")
+    @JsonProperty("folderId")
     private Long folderId;
 
     @NotNull
     @Schema(description = "전략 ID", example = "10")
+    @JsonProperty("strategyId")
     private Long strategyId;
 }
