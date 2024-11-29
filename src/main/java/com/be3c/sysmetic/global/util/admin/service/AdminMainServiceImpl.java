3193a21a66809b1c991d01290c15095d82b621d1
Eseas <8snlf9@gmail.com>
2024-11-29 10:46:02 +0900

    feat: 메서드 분리
    완료
    - 메인 페이지 각 데이터 호출 메서드 분리

--- src/main/java/com/be3c/sysmetic/global/util/admin/service/AdminMainServiceImpl.java
+++ src/main/java/com/be3c/sysmetic/global/util/admin/service/AdminMainServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -41,41 +42,21 @@ public class AdminMainServiceImpl implements AdminMainService {
 
     @Override
     public AdminMainResponseDto getAdminMain() {
-        Long inquiryCount = inquiryRepository.count();
-        Long answeredInquiry = inquiryRepository.findAnsweredInquiry();
-        Long userCount = memberRepository.countUser();
-        Long traderCount = memberRepository.countTrader();
-        Long managerCount = memberRepository.countManager();
-        Long strategyCount = strategyRepository.count();
-        Long openStrategyCount = strategyRepository.countOpenStatus();
-        Long approvalOpenStrategyCount = strategyApprovalRepository.countWaitingStrategyCount();
-
-        Pageable pageable = PageRequest.of(0, 5);
-
         return AdminMainResponseDto.builder()
-                .runReportResponseDto(RunReportResponseDto.builder()
-                        .allReportResponse(getReport(LocalDate.of(2000, 1, 1), LocalDate.now())) // 전체 기간
-                        .monthlyReportResponse(getReport(LocalDate.now().minusMonths(1), LocalDate.now())) // 최근 1개월
-                        .weeklyReportResponse(getReport(LocalDate.now().minusWeeks(1), LocalDate.now())) // 최근 1주
-                        .dayReportResponse(getReport(LocalDate.now().minusDays(1), LocalDate.now())) // 최근 1일
-                        .build())
-                .strategyCount(StrategyCountResponseDto.builder()
-                        .totalStrategyCount(strategyCount)
-                        .openStrategyCount(openStrategyCount)
-                        .waitingStrategyCount(approvalOpenStrategyCount)
-                        .build())
-                .memberCount(MemberCountResponseDto.builder()
-                        .userMemberCount(userCount)
-                        .TraderMemberCount(traderCount)
-                        .ManagerMemberCount(managerCount)
-                        .totalMemberCount(userCount + traderCount)
-                        .build())
-                .adminNoticeResponse(noticeRepository.findAdminMainNotice(pageable).getContent())
-                .adminInquiryResponseDto(AdminInquiryResponseDto.builder()
-                        .inquiryCount(inquiryCount)
-                        .answeredInquiryCount(answeredInquiry)
-                        .waitingInquiryCount(inquiryCount - answeredInquiry)
-                        .build())
+                .runReportResponseDto(getRunReportResponseDto())
+                .strategyCount(getStrategyCount())
+                .memberCount(getMemberRatio())
+                .adminNoticeResponse(getAdminNoticeResponseDto())
+                .adminInquiryResponseDto(getInquiryResponseDto())
+                .build();
+    }
+
+    private RunReportResponseDto getRunReportResponseDto() {
+        return RunReportResponseDto.builder()
+                .allReportResponse(getReport(LocalDate.of(2000, 1, 1), LocalDate.now())) // 전체 기간
+                .monthlyReportResponse(getReport(LocalDate.now().minusMonths(1), LocalDate.now())) // 최근 1개월
+                .weeklyReportResponse(getReport(LocalDate.now().minusWeeks(1), LocalDate.now())) // 최근 1주
+                .dayReportResponse(getReport(LocalDate.now().minusDays(1), LocalDate.now())) // 최근 1일
                 .build();
     }
 
@@ -99,10 +80,44 @@ public class AdminMainServiceImpl implements AdminMainService {
     }
 
     private MemberCountResponseDto getMemberRatio() {
+        Long userCount = memberRepository.countUser();
+        Long traderCount = memberRepository.countTrader();
+        Long managerCount = memberRepository.countManager();
+
         return MemberCountResponseDto.builder()
-                .totalMemberCount(memberRepository.count())
-                .TraderMemberCount(memberRepository.countTrader())
-                .userMemberCount(memberRepository.countUser())
+                .userMemberCount(userCount)
+                .TraderMemberCount(traderCount)
+                .ManagerMemberCount(managerCount)
+                .totalMemberCount(userCount + traderCount)
+                .build();
+    }
+
+    private AdminInquiryResponseDto getInquiryResponseDto() {
+        Long inquiryCount = inquiryRepository.count();
+        Long answeredInquiry = inquiryRepository.findAnsweredInquiry();
+
+        return AdminInquiryResponseDto.builder()
+                .inquiryCount(inquiryCount)
+                .answeredInquiryCount(answeredInquiry)
+                .waitingInquiryCount(inquiryCount - answeredInquiry)
+                .build();
+    }
+
+    private List<AdminNoticeResponseDto> getAdminNoticeResponseDto() {
+        Pageable pageable = PageRequest.of(0, 5);
+
+        return noticeRepository.findAdminMainNotice(pageable).getContent();
+    }
+
+    private StrategyCountResponseDto getStrategyCount() {
+        Long strategyCount = strategyRepository.count();
+        Long openStrategyCount = strategyRepository.countOpenStatus();
+        Long approvalOpenStrategyCount = strategyApprovalRepository.countWaitingStrategyCount();
+
+        return StrategyCountResponseDto.builder()
+                .totalStrategyCount(strategyCount)
+                .openStrategyCount(openStrategyCount)
+                .waitingStrategyCount(approvalOpenStrategyCount)
                 .build();
     }
 }
