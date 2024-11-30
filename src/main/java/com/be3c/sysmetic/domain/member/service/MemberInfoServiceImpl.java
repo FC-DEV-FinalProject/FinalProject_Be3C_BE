317d704437281f61b3191e87df8ccd750394b42b
Eseas <95269141+Eseas@users.noreply.github.com>
2024-11-30 17:33:38 +0900

    fix: MemberInfoServiceImpl.java

--- src/main/java/com/be3c/sysmetic/domain/member/service/MemberInfoServiceImpl.java
+++ src/main/java/com/be3c/sysmetic/domain/member/service/MemberInfoServiceImpl.java
@@ -128,7 +128,7 @@ public class MemberInfoServiceImpl implements MemberInfoService {
         Member member = findMemberById(userId);
 
         if(file != null) {
-            fileService.uploadImage(file, new FileRequest(FileReferenceType.MEMBER, member.getId()));
+            fileService.updateImage(file, new FileRequest(FileReferenceType.MEMBER, member.getId()));
         }
 
         if(memberPatchInfoRequestDto.getNicknameDuplCheck() &&
