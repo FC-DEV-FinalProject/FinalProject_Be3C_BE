29a96279b7281aed7c0f2f728abd51c81567709c
한지선 <seon.hannn@gmail.com>
2024-12-01 13:16:14 +0900

    fix: user 권한 검증 로직 수정

--- src/main/java/com/be3c/sysmetic/domain/strategy/service/DailyServiceImpl.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/service/DailyServiceImpl.java
@@ -174,13 +174,16 @@ public class DailyServiceImpl implements DailyService {
      */
     @Override
     public PageResponse<DailyGetResponseDto> findTraderDaily(Long strategyId, Integer page, LocalDate startDate, LocalDate endDate) {
+        Strategy exitingStrategy = strategyRepository.findById(strategyId).orElseThrow(() ->
+                new StrategyBadRequestException(StrategyExceptionMessage.DATA_NOT_FOUND.getMessage(), ErrorCode.NOT_FOUND));
+
         Pageable pageable = PageRequest.of(page, 10);
 
         String userRole = securityUtils.getUserRoleInSecurityContext();
 
         // trader일 경우, 본인의 전략인지 검증
         if(userRole.equals("TRADER")) {
-           validUser(strategyId);
+           validUser(exitingStrategy.getTrader().getId());
         }
 
         // member일 경우, 권한 없음 처리
