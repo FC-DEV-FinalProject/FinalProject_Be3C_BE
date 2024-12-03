e1b37bd2faab9d28303a5b12258093040bb4f4bc
mminn20 <mminnkk20@gmail.com>
2024-12-03 13:42:44 +0900

    메인 페이지 대표전략 평균지표 DTO 추가 #203

---
+++ src/main/java/com/be3c/sysmetic/domain/strategy/dto/MainPageAnalysisDto.java
@@ -0,0 +1,25 @@
+package com.be3c.sysmetic.domain.strategy.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class MainPageAnalysisDto {
+
+    /*
+        MainPageAnalysisDto : 메인 페이지 대표전략 분석지표 응답용 DTO
+
+        smScoreTopStrategyName : SM Score 1위인 전략명
+        xAxis : 날짜
+        averageStandardAmount : 통합 기준가
+        smScore : smScore 1위의 누적 손익률
+    */
+
+    private String smScoreTopStrategyName;
+    private List<String> xAxis;
+    private List<Double> averageStandardAmount;
+    private List<Double> accumProfitLossRate;
+}
