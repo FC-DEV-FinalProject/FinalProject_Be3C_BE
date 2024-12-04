f808922e2c70b8ce220f1fd2bbe3c3cd99e72501
Eseas <8snlf9@gmail.com>
2024-12-04 19:58:52 +0900

    fix: 5차 수정

--- src/main/java/com/be3c/sysmetic/domain/strategy/service/StockServiceImpl.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/service/StockServiceImpl.java
@@ -146,8 +146,6 @@ public class StockServiceImpl implements StockService {
                         USING_STATE.getCode()
                 ).orElseThrow(EntityNotFoundException::new);
 
-        log.info("stockRequestDto.getName {}", stockPutRequestDto.getName());
-
         /*
             같은 이름이 존재 = true    같은 이름이 미존재 = false
             원래 이름과 같음 = true    원래 이름과 같지 않음 = false
