6a7b0d9fe9cf63c6e3f041c1b0dec5052a6c4dd5
mminn20 <mminnkk20@gmail.com>
2024-12-04 14:51:19 +0900

    엔드 포인트 추가 #203

--- src/main/java/com/be3c/sysmetic/domain/strategy/controller/MainPageControllerDocs.java
+++ src/main/java/com/be3c/sysmetic/domain/strategy/controller/MainPageControllerDocs.java
@@ -2,14 +2,10 @@ package com.be3c.sysmetic.domain.strategy.controller;
 
 import com.be3c.sysmetic.domain.strategy.dto.MainPageAnalysisDto;
 import com.be3c.sysmetic.domain.strategy.dto.MainPageDto;
-import com.be3c.sysmetic.domain.strategy.dto.StrategyAnalysisResponseDto;
 import com.be3c.sysmetic.global.common.response.APIResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "메인 페이지 API", description = "메인 페이지 조회")
